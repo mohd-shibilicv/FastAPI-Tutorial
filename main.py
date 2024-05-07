017c28fd695cc9c213b569e429952a2f5af4e66a
mohammed shibili cv <mohshibilicv@gmail.com>
2024-05-07 10:17:13 +0530

    Lesson: Query Parameters

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    email: str
+    name: Optional[str] = None
+    age: int
+    mobile_number: Optional[int] = None
+
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+
+@app.post("/users/")
+async def create_user(user: User):
+    return user
+
+
+@app.get("/users/{user_id}")
+async def user_details(user_id: int, q: str | None = None, short: bool = False):
+    user = {"user_id": user_id}
+    if q:
+        user.update({
+            "q": q
+        })
+    if short:
+        user.update({
+            "description": "This is an amazing user that has a long description"
+        })
+    return user
